5b22891c20809c333f46f92f4919361168d503a9
Artem Draft <artemon_93@mail.ru>
2021-04-30 19:19:19 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@
 from setuptools import setup
 
 setup(name='pylgnetcast',
-            version='0.3.1',
+            version='0.3.2',
             description='Client for the LG Smart TV running NetCast 3 or 4.',
-            url='https://github.com/wokar/pylgnetcast',
+            url='https://github.com/Drafteed/python-lgnetcast',
             license='MIT',
             packages=['pylgnetcast'],
             install_requires=[],
